5eb9571019abd7dc81ec51e0c95ea7060e217ce6
Rosa <rreynarive1@educacion.navarra.es>
2022-12-21 20:05:03 +0100

    4 aFIN

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,12 +1,9 @@
 import urllib.request
-letra = 0
+
 def leer_url(url):
     file = urllib.request.urlopen(url)
     for linea in file:
         linea_deco = linea.decode("utf-8")
-        for letra in linea:
-            letra += 1
-            print(letra)
         print(linea_deco)
     return
 
